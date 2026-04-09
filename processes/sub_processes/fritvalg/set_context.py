7701be584e040053a5dd9f0b8da0d47e2f3b6383
Rune Holm Ustrup <rku@aarhus.dk>
2026-04-09 13:21:30 +0200

    feat(subprocess): Add fritvalg subprocess for digital blanket processing

---
+++ processes/sub_processes/fritvalg/set_context.py
@@ -0,0 +1,27 @@
+"""Module to set context values for processing"""
+
+from helpers import config as helper_config
+from helpers.context_functions import set_context_values
+
+from . import config
+
+
+def set_context_vars(item_data: dict, item_reference: str, item_id: str):
+    """Set context values based on item data"""
+
+    set_context_values(
+        os2_forms_url=item_data.get("url", ""),
+        reference=item_reference,
+        cpr=item_data.get("cpr", ""),
+        clinic_name=item_data.get("klinik_navn", ""),
+        clinic_address=item_data.get("klinik_adresse", ""),
+        # clinic_phone_number=item_data.get("klinik_telefonnummer", ""),
+        # clinic_provider_number=item_data.get("klinik_ydernummer", ""),
+        form_data=item_data.get("form_data", ""),
+        consent=bool(item_data.get("journal_samtykke")),
+        work_item=item_id,
+        document_file_path=helper_config.DOCUMENT_PATH,
+        document_file_name=config.DOCUMENT_FILE_NAME,
+        process_name=config.DASHBOARD_PROCESS_NAME,
+        current_step_name=None,
+    )
--- processes/sub_processes/tilflytter/config.py
+++ processes/sub_processes/tilflytter/config.py
@@ -1,3 +1,5 @@
+"""Configuration settings for the Tilflytter process."""
+
 # ----------------------
 # Document handling settings
 # ----------------------
